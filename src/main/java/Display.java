939fde685f978989c2eb27f864627d655bfc3201
Kuba Bigaj <k.bigaj666@gmail.com>
2022-05-25 13:43:06 +0200

    V1.0

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,15 +1,32 @@
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+
 import javax.swing.*;
 import java.net.URL;
 
 public class Display {
     JFrame window;
     JLabel image;
+    int index=0;
+    Listing<Submission> posts;
 
-    public Display(String title) {
-        window = new JFrame(title);
+    public Display(Listing<Submission> posts) {
+        window = new JFrame();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         image = new JLabel();
         window.add(image);
+
+        this.posts=posts;
+    }
+
+    public void advance(){
+        this.setTitle(posts.get(index).getTitle());
+        this.show(posts.get(index).getUrl());
+        index++;
+    }
+
+    public void setTitle(String title){
+        window.setTitle(title);
     }
 
     public void show(String url) {
@@ -21,5 +38,7 @@ public class Display {
             e.printStackTrace();
         }
     }
+
+
 
 }
